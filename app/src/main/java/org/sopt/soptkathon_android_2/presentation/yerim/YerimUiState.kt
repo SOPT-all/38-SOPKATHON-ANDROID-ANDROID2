0f12dfd365e870332ae4781a9f3255a629ea9d9e
yerim0829 <123yellly@gmail.com>
2026-05-17 05:37:22 +0900

    [feat/#16] 진단결과 UI

--- app/src/main/java/org/sopt/soptkathon_android_2/presentation/yerim/YerimUiState.kt
+++ app/src/main/java/org/sopt/soptkathon_android_2/presentation/yerim/YerimUiState.kt
@@ -1,5 +1,13 @@
 package org.sopt.soptkathon_android_2.presentation.yerim
 
+import androidx.compose.runtime.Composable
+
 data class YerimUiState(
     val dummy: String = "",
+
+    val isLoading : Boolean = true,
+    val typeLabel: String = "",
+    val description: String= "",
 )
+
+
